bb7971aacbc0d86b6bb712d9ec2f9d230c30993a
Jason Barto <jason.p.barto@gmail.com>
2025-08-30 01:42:15 +0100

    added unit tests to build step

---
+++ tests/unit_test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+docker run -d --name prime-test -p 8080:8080 prime-service | tee results.log
+
+sleep 3
+
+curl -s 'http://localhost:8080/primes?number=10' | jq -c '.primes' | grep -E '^\[2,3,5,7\]$'
+if [ $? -eq 0 ]; then
+    echo "Test passed"
+    docker stop $(docker ps -a -q --filter ancestor=prime-service:latest --format="{{.ID}}")
+else
+    echo "Primes service did not return expected result"
+    docker stop $(docker ps -a -q --filter ancestor=prime-service:latest --format="{{.ID}}")
+    exit 1
+fi
